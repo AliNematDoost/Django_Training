dabfb5e708024b225444eba6c478502843bc6566
AliND <alinematdoost82@gmail.com>
2025-09-15 16:25:18 -0700

    template engin

--- base/views.py
+++ base/views.py
@@ -9,8 +9,21 @@ from django.http import HttpResponse
 # def room(request):
 #     return HttpResponse("this is the room page!")
 
+rooms = [
+    {'id':1, 'title':'Linear Algebra'},
+    {'id':2, 'title':'AI'},
+    {'id':3, 'title':'DataBase'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    # render output should be like this : render(request, template_name, context)
+    # which context should be dictionary
+    context = {
+        'rooms' : rooms
+    }
+    # separated the templates of base app from the main templates folder
+    # we should mention 'base' because home.html is a template of base app
+    return render(request, 'base/home.html', context)
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
